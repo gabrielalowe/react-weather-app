1c797b710bb23f668de59b826f1545b9f7322589
Gabriela <gabriela.oz@yahoo.com>
2023-06-26 19:45:50 +0300

    Added Boostrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by Gabriela Lefter and it is open source on{" "}
-        <a
-          href="https://github.com/gabrielalowe/weather-react"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          GitHub
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Gabriela Lefter and it is open source on{" "}
+          <a
+            href="https://github.com/gabrielalowe/weather-react"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
