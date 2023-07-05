c3fa6821d341ebf2dde56f6ba3a72343bb3af89f
Gabriela <gabriela.oz@yahoo.com>
2023-07-05 22:04:47 +0300

    implemet the apiUrl for the forecast data

---
+++ src/WeatherForecast.js
@@ -0,0 +1,31 @@
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "61da32cd216c8fce43a31b240f1cadcf";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordintes.lat;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
